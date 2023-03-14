2bf17ee344438b6097fdf91a859beeef5dc1a7e5
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-14 18:42:00 +0100

    mtchewww

---
+++ app/about/page.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+
+const PageAbout = () => {
+  return (
+    <div className="p-4">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+        architecto alias dolores a laboriosam ab, quo eligendi odio qui quas est
+        consequuntur iure quidem perferendis enim optio, aspernatur
+        reprehenderit vel commodi eius nihil eos amet. Temporibus, autem ipsa
+        consequuntur eligendi accusantium eius eum sunt, aspernatur obcaecati
+        adipisci iure at sint recusandae incidunt. Nam sit, maxime distinctio
+        modi aperiam vel, sint eum porro odio deserunt totam praesentium
+        obcaecati iste at quisquam. Ullam possimus natus architecto nam, atque
+        magnam velit ea, ut minima reprehenderit corporis deleniti optio
+        dignissimos cumque illo! Deleniti, voluptas corporis, eligendi quasi
+        perspiciatis placeat saepe nisi fuga, nemo impedit soluta consequuntur
+        voluptate aliquid repellat sed repudiandae distinctio adipisci non odio
+        totam? Ratione corrupti quisquam et necessitatibus facere aut
+        praesentium beatae minus veritatis delectus cupiditate hic ut tempora
+        optio at recusandae, accusamus minima veniam? Aspernatur quae libero
+        nulla blanditiis, fuga minus earum nesciunt optio cupiditate
+        perspiciatis eligendi voluptates mollitia inventore corrupti ex omnis
+        debitis totam! Libero maxime, delectus cumque praesentium unde voluptas
+        iure nemo magni optio, accusantium, ab id ut ipsam placeat debitis modi
+        exercitationem saepe minima error dolores corrupti consequatur
+        excepturi. Iste, dolorum. Commodi, quasi? Ex distinctio asperiores
+        ratione, veritatis amet vero dolorum qui assumenda magnam nam odit
+        suscipit ducimus beatae earum consectetur. Modi numquam quos nisi
+        adipisci eligendi ipsam similique quis tempora saepe quam, omnis soluta.
+        At iusto ratione incidunt soluta distinctio illo quo, dicta beatae
+        omnis, temporibus et autem aliquid quod! Aspernatur debitis voluptatem
+        sed. Eligendi alias facilis, neque perspiciatis saepe enim expedita eius
+        eum fuga. Qui eaque enim illo asperiores libero voluptatum ducimus nihil
+        cum, non mollitia velit vitae repellat possimus inventore blanditiis,
+        aspernatur iusto aut perspiciatis explicabo numquam voluptates. Fugit ex
+        nihil quia tempora aperiam expedita sequi in. Tempora ab, ullam
+        repellendus iste soluta non dolore dolorem? Laboriosam amet explicabo
+        architecto optio veritatis eaque pariatur, autem totam in rem itaque,
+        officia rerum voluptatum ratione ea, et consequuntur corporis veniam
+        aspernatur. Id rem, fugiat asperiores libero facere necessitatibus
+        consequatur harum natus animi similique suscipit quo, molestiae itaque
+        tempora nobis aspernatur beatae temporibus nulla. Officia obcaecati fuga
+        atque. Atque obcaecati et sed magnam rem doloremque consequatur
+        cupiditate iusto ducimus itaque, unde ea at autem earum fuga perferendis
+        amet. Quas vero consequatur dicta vel provident reprehenderit nulla amet
+        suscipit debitis veniam quae exercitationem ex temporibus, aperiam unde
+        quaerat ut in officia magni deserunt similique explicabo consectetur?
+        Perspiciatis natus nesciunt, eaque sequi laboriosam rerum modi
+        architecto doloribus nulla, labore dolorum nam quasi atque quos placeat
+        ipsum eligendi aspernatur? Laudantium, esse pariatur veniam qui ea
+        excepturi deleniti maxime cupiditate iusto, voluptatibus, iure
+        exercitationem. Quos quod nisi placeat, eveniet aliquam voluptate aut
+        quis vel eaque ratione aliquid iusto, laborum mollitia illo. Ut
+        assumenda voluptatibus veritatis fuga animi? Qui rerum accusamus quam
+        dolor quibusdam quia deserunt, consectetur natus facilis error repellat,
+        quis debitis amet distinctio illo sequi eos. Excepturi harum voluptas,
+        suscipit debitis voluptatum architecto temporibus saepe tempora eos
+        voluptates at exercitationem porro aut perspiciatis! Impedit placeat
+        praesentium non doloremque dolores et aspernatur deserunt eligendi. Cum
+        ipsam blanditiis commodi ea facilis neque reprehenderit animi cumque
+        doloremque nihil maiores ut est debitis beatae nesciunt aperiam soluta
+        eos ipsum doloribus tenetur, harum enim. Minima doloremque sint libero
+        ut nam aspernatur quas. Accusamus consequuntur eum quos quis impedit rem
+        velit amet est vitae explicabo tenetur ex nisi porro dicta, accusantium
+        libero perspiciatis minima ducimus fugit reprehenderit nihil similique.
+        Officia nisi incidunt optio earum assumenda id ea molestias. Quaerat
+        officia veniam, consequatur ex laboriosam voluptatem odit quibusdam eos
+        in aut deleniti quae! Labore rem dicta omnis quasi et odit officia quos
+        quod maxime consequatur. Quod odit, temporibus quidem optio mollitia
+        dolores, esse nam quisquam, saepe ea quo aspernatur modi necessitatibus.
+        Reiciendis quo itaque harum libero tempora repudiandae quos dicta enim.
+        Aut expedita aperiam commodi voluptates optio neque dolore amet aliquam,
+        quo nulla, temporibus, nostrum quos quia reprehenderit repellendus
+        soluta vel? Maxime aspernatur explicabo iste. Dicta veniam ullam
+        deleniti velit, qui a. Dolore possimus tenetur fugit deserunt at
+        aspernatur esse perferendis non voluptas unde reprehenderit est omnis
+        blanditiis ab sapiente quaerat exercitationem, ex reiciendis laboriosam
+        labore libero? Quod qui suscipit similique amet totam numquam at,
+        tenetur nobis eos a iste ipsa asperiores facere recusandae labore
+        repellat illum nesciunt. Maxime quibusdam rerum ratione ex, deserunt
+        magni beatae magnam tempora, nisi illo et. Dolorum nemo accusantium
+        ratione aut asperiores, repellendus laboriosam eveniet provident ad
+        magni nesciunt qui alias illo reiciendis non voluptates voluptatem vel!
+        Sit ab omnis aperiam non unde, modi vel inventore accusamus. Similique
+        illo ipsa odit, ad quibusdam distinctio odio rem tenetur harum id.
+        Quidem quasi totam magnam, est laudantium dolore facere, amet
+        dignissimos accusamus accusantium sapiente optio nam? Eaque, libero sunt
+        eius incidunt dolore provident eum unde ipsa iure minus animi sequi
+        praesentium facilis, aliquam, quasi deleniti repellat commodi odio
+        pariatur aliquid debitis quis maxime perspiciatis est? Asperiores
+        aliquam quasi nulla fuga quae explicabo sint maiores perferendis eum
+        quia repudiandae culpa excepturi, ut temporibus, facilis hic, sequi
+        corrupti? Nobis id iusto ducimus fugit illum dicta aperiam fuga impedit
+        sed! Aliquam temporibus numquam voluptate quidem asperiores ratione
+        doloribus, est sint labore mollitia nesciunt nobis excepturi corrupti,
+        deserunt accusantium impedit voluptatem dolores vel expedita suscipit,
+        aspernatur quae ipsam magni dolore! Incidunt similique quibusdam
+        doloribus voluptatem nam minus corporis porro voluptate facere cum et
+        dolorem placeat, animi esse mollitia asperiores sed molestiae excepturi
+        ut blanditiis in tempore. Ducimus adipisci fuga aperiam! Quam ipsum, ut
+        aspernatur, ratione accusantium est odio quia culpa eius neque libero
+        delectus fugit nihil quos excepturi illum eveniet consectetur quaerat
+        beatae reprehenderit repudiandae et aliquid vel! Quos, esse! Aspernatur
+        deleniti ad illo totam voluptatibus animi praesentium reiciendis
+        corrupti voluptatem possimus dicta velit enim officia, perferendis
+        itaque quam suscipit et nam architecto? Necessitatibus cum quasi fugiat
+        dolor distinctio, odit tenetur perspiciatis veniam hic repellat
+        exercitationem aspernatur, harum commodi accusantium minus.
+        Necessitatibus, laborum. Dicta laudantium illum consectetur ab nulla
+        sint aliquid ullam voluptatem libero dolore maxime rem quo, hic nobis
+        consequatur obcaecati enim sapiente magni placeat a. Sint, excepturi
+        dignissimos error laboriosam ex possimus consequatur enim eaque
+        perferendis, quos adipisci sapiente deleniti exercitationem sunt
+        accusantium tempora perspiciatis nisi.
+      </h1>
+      <button
+        className="btn sticky bottom-0"
+        // style={{
+        //   position: "sticky",
+        //   bottom: 0,
+        // }}
+      >
+        Button
+      </button>
+    </div>
+  );
+};
+
+export default PageAbout;
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const Page = () => {
   const [threads, setThreads] = useState(thread);
   return (
-    <div className="w-[60%] pt-5 mx-auto min-h-screen relative">
+    <div className="w-[60%] pt-5 mx-auto h-screen">
       <div className="">
         {threads.map((message) => (
           <div className="chat chat-start">
@@ -14,22 +14,17 @@ const Page = () => {
                 <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </div>
-            <div className="chat-header">
-              Obi-Wan Kenobi
-              <time className="text-xs opacity-50">12:45</time>
-            </div>
+            <div className="chat-header">Obi-Wan Kenobi</div>
             <div className="chat-bubble">You were the Chosen One!</div>
-            <div className="chat-footer opacity-50">Delivered</div>
           </div>
         ))}
       </div>
-      <div className="absolute bottom-0 w-full my-4 flex justify-center">
+      <div className="sticky bottom-0  w-full my-4 flex justify-center">
         <form className="flex items-center justify-start w-full gap-2">
           <textarea
             className="resize-y min-h-0 h-auto border rounded-md w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline max-h-96"
             placeholder="Enter your text here..."
           ></textarea>
-
           <button className="bg-purple-600 px-4  rounded-md text-white font-semibold">
             Send
           </button>
